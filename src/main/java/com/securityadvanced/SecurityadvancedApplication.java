1d2751034d1ea8ab7bac4f0a7d76531fdb7bf04e
hyuk2 <hyuk2000s@gmail.com>
2025-01-24 15:29:53 +0900

    first commit

---
+++ src/main/java/com/securityadvanced/SecurityadvancedApplication.java
@@ -0,0 +1,13 @@
+package com.securityadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityadvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityadvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/securityadvanced/SecurityadvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securityadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityadvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
