70712b0c3db0a6119ed1371c7771f37a1ec5cf4c
hyuk2 <hyuk2000s@gmail.com>
2025-01-24 16:53:20 +0900

    study formlogin

--- src/main/java/com/securityadvanced/IndexController.java
+++ src/main/java/com/securityadvanced/IndexController.java
@@ -12,4 +12,14 @@ public class IndexController {
         return "index";
     }
 
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+
 }
