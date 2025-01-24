004714cb8d640c974772d8ebbf2558d0b7a88eed
hyuk2000s <hyuk2000s@naver.com>
2025-01-24 21:43:33 +0900

    http basic 기본인증

--- src/main/java/com/securityadvanced/SecurityConfig.java
+++ src/main/java/com/securityadvanced/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
 import java.io.IOException;
 
@@ -30,22 +31,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(form -> form
-//                        .loginPage("/loginPage")
-                        .loginProcessingUrl("/loginProc")
-                        .defaultSuccessUrl("/", false)
-                        .failureUrl("/failed")
-                        .usernameParameter("userId")
-                        .passwordParameter("passwd")
-                        .successHandler((request, response, authentication) -> {
-                            System.out.println("authentication : " + authentication);
-                            response.sendRedirect("/home");
-                        })
-                        .failureHandler((request, response, exception) -> {
-                            System.out.println("exception : " + exception);
-                            response.sendRedirect("/login");
-                        })
-                        .permitAll());
+                .httpBasic(basic -> basic.authenticationEntryPoint(
+                        new CustomAuthenticationEntryPoint()));
 
         return http.build();
     }
